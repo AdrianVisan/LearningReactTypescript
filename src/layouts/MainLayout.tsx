9e4acc62c178639bf8f4557966e270160c2421ca
Adrian Visan <visan.adrianflorin@gmail.com>
2024-12-24 16:15:45 +0200

    Add NavBar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import NavBar from '../components/NavBar';
 
 function MainLayout() {
   return (
     <>
+      <NavBar />
       <Outlet />
       <ToastContainer />
     </>
