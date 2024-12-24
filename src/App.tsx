636ce90ba4c42cc0ccc2f0ec0c57ccb602d6c5a4
Adrian Visan <visan.adrianflorin@gmail.com>
2024-12-24 16:05:15 +0200

    Initialize React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
