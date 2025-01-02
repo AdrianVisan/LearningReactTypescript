3810acd6401eda9c2abce541d9180398bc21c1e6
Adrian Visan <visan.adrianflorin@gmail.com>
2025-01-03 00:34:34 +0200

    Small cleanup

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,7 @@
-import { Box, Button, Typography } from '@mui/material';
 import { WarningAmberRounded } from '@mui/icons-material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function NotFoundPage() {
   return (
