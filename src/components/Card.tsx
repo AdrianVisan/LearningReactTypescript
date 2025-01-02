2829392dbdb131db5a35eda8a4db678d07578e75
Adrian Visan <visan.adrianflorin@gmail.com>
2025-01-02 21:31:52 +0200

    Migrate Card component to MUI

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,19 @@
+import { Box } from '@mui/material';
 import { ReactNode } from 'react';
 
 type CardProps = { children: ReactNode[]; highlighted?: boolean };
 
 function Card({ children, highlighted = false }: CardProps) {
   return (
-    <div
-      className={`${
-        highlighted ? 'bg-indigo-100' : ''
-      } p-6 rounded-lg shadow-md`}>
+    <Box
+      sx={{
+        backgroundColor: `${highlighted ? 'lightsteelblue' : ''}`,
+        boxShadow: 2,
+        padding: 2,
+        borderRadius: 2,
+      }}>
       {children}
-    </div>
+    </Box>
   );
 }
 export default Card;
